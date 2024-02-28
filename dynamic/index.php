ac6c6ddc66a7c862aee5e45018d7d195dbc6df3f
Lari Nurkka <lari.nurkka@gmail.com>
2024-02-28 18:13:34 +0200

    Secure redirect

---
+++ dynamic/index.php
@@ -0,0 +1,9 @@
+<?php
+
+if (!isset($_SERVER['HTTPS']) or $_SERVER['HTTPS'] == 'off' ) {
+    $redirect_url = "https://" . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+    header("Location: $redirect_url");
+    exit();
+}
+
+require "index.html";
