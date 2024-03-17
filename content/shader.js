ce1cd8be244b68d1840dc5aa075b35045ffbb529
Lari Nurkka <lari.nurkka@gmail.com>
2024-03-17 20:29:12 +0200

    Fix: actually load shaders from shader files

--- content/shader.js
+++ content/shader.js
@@ -76,69 +76,24 @@ const getPrimaryAccent = () => {
 };
 
 
-window.onload = () => {
+window.onload = async () => {
     gl = canvas.getContext("webgl2");
     if (!gl) return;
+
+    const getShaderSource = url => fetch(url).then(response => response.text());
 
     shaderProgram = buildShaderProgram([
         {
             type: gl.VERTEX_SHADER,
-            code: `#version 300 es
-            in vec4 a_position;
-
-            void main() {
-                gl_Position = a_position;
-            }`
+            code: await getShaderSource(
+                document.querySelector('script[type="x-shader/x-vertex"]').src
+            )
         },
         {
             type: gl.FRAGMENT_SHADER,
-            code: `#version 300 es
-            precision highp float;
-
-            uniform vec2 u_resolution;
-            uniform float u_time;
-            uniform vec3 u_primary_accent;
-            
-            out vec4 outColor;
-            
-            vec3 hash33(vec3 p3)
-            {
-                p3 = fract(p3 * vec3(443.8975,397.2973, 491.1871));
-                p3 += dot(p3, p3.yxz+19.19);
-                return -1.0 + 2.0 * fract(vec3((p3.x + p3.y)*p3.z, (p3.x+p3.z)*p3.y, (p3.y+p3.z)*p3.x));
-            }
-        
-            float simplex_noise(vec3 p)
-            {
-                const float K1 = 0.333333333;
-                const float K2 = 0.166666667;
-                
-                vec3 i = floor(p + (p.x + p.y + p.z) * K1);
-                vec3 d0 = p - (i - (i.x + i.y + i.z) * K2);
-                
-                // thx nikita: https://www.shadertoy.com/view/XsX3zB
-                vec3 e = step(vec3(0.0), d0 - d0.yzx);
-                vec3 i1 = e * (1.0 - e.zxy);
-                vec3 i2 = 1.0 - e.zxy * (1.0 - e);
-                
-                vec3 d1 = d0 - (i1 - 1.0 * K2);
-                vec3 d2 = d0 - (i2 - 2.0 * K2);
-                vec3 d3 = d0 - (1.0 - 3.0 * K2);
-                
-                vec4 h = max(0.6 - vec4(dot(d0, d0), dot(d1, d1), dot(d2, d2), dot(d3, d3)), 0.0);
-                vec4 n = h * h * h * h * vec4(dot(d0, hash33(i)), dot(d1, hash33(i + i1)), dot(d2, hash33(i + i2)), dot(d3, hash33(i + 1.0)));
-                
-                return dot(vec4(31.316), n);
-            }
-
-            void main() {
-                const float TIMESCALE = 0.05;
-
-                vec2 a = vec2(gl_FragCoord.xy / u_resolution);
-                float simplex = clamp(1.0 - simplex_noise(vec3(a.x, a.y, u_time * TIMESCALE)), 0.0, 1.0);
-                vec3 color = vec3(simplex) * u_primary_accent;
-                outColor = vec4(mix(u_primary_accent, color, min(u_time * 0.3, 1.0)), 1);
-            }`
+            code: await getShaderSource(
+                document.querySelector('script[type="x-shader/x-fragment').src
+            )
         }
     ]);
 
