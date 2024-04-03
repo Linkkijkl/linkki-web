3264e1651a978ace8c1d6cdfd6f9ecfe57625304
Lari Nurkka <lari.nurkka@gmail.com>
2024-04-03 22:41:55 +0300

    Basic working theme toggle

--- assets/js/lightswitch.js
+++ assets/js/lightswitch.js
@@ -1,20 +1,54 @@
-const toggle = () => {
-    console.log("Theme toggle");
-};
+let icon;
+let darkStyleObject;
+let darkStyleParent;
+
+const isDark = () => {
+    const dm = window.localStorage.getItem("darkmode");
+    if (dm === null) return window.matchMedia("(prefers-color-scheme: dark)").matches;
+    return dm === "true";
+}
+
+
+const setDark = dark => {
+    const ds = document.querySelector("#darkstyle");
+    if (dark && !ds) {
+        darkStyleParent.appendChild(darkStyleObject.cloneNode());
+        icon.className = "fas fa-2x fa-sun";
+    } else if (!dark && ds) {
+        ds.remove();
+        icon.className = "fas fa-2x fa-moon";
+    }
+    window.localStorage.setItem("darkmode", dark);
+}
+
+
+const toggle = () => setDark(!isDark());
+
 
 const createButton = () => {
     const element = document.querySelector(".home-carousel, #heading-breadcrumbs");
 
-    let icon = document.createElement("i");
-    icon.className = "fas fa-2x fa-sun";
+    icon = document.createElement("i");
 
     let lightswitch = document.createElement("a");
     lightswitch.appendChild(icon);
     lightswitch.style = "position: absolute; top: 10px; right: 10px;";
+    lightswitch.onclick = (event) => {
+        event.preventDefault();
+        toggle();
+    };
 
     element.appendChild(lightswitch);
 }
 
-window.addEventListener("load", () => {
+
+window.addEventListener("DOMContentLoaded", () => {
     createButton();
+
+    const darkStyle = document.querySelector("#darkstyle");
+    darkStyle.media = "";
+    darkStyleObject = darkStyle.cloneNode();
+    darkStyleParent = darkStyle.parentElement;
+
+    setDark(isDark());
 });
