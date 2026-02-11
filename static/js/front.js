10648b23bde64b75c8f5c350a5e39473e67a1567
Lari Nurkka <lari.nurkka@gmail.com>
2026-02-11 18:38:59 +0200

    Carousel update (#354)
    * center carousel content vertically
    
    * annual gala carousel advert
    
    * rewrite some carousel content
    
    * stylize carousel dots
    
    * fix dragging carousel on mobile not stopping autoscroll
    
    * fix carousel layout in smaller screens

--- static/js/front.js
+++ static/js/front.js
@@ -36,7 +36,7 @@ const sliders = async () => {
     draggable: true,
     scrollLock: true,
     rewind: true,
-    duration: 3,
+    duration: 2,
     dots: '.dots',
   });
 
@@ -45,7 +45,8 @@ const sliders = async () => {
   let scrolled = false;
   let mouseOnTop = false;
 
-  gliderElement.addEventListener('mousedown', () => scrolled = true);
+  gliderElement.addEventListener('click', () => scrolled = true);
+  gliderElement.addEventListener('scroll', () => scrolled = true);
   document.querySelector('.dots').addEventListener('mousedown', () => scrolled = true);
   gliderElement.addEventListener('mouseenter', () => mouseOnTop = true);
   gliderElement.addEventListener('mouseleave', () => mouseOnTop = false);
